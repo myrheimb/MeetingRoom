248853f56dc8082648dcce36c2f0e092a56b9fa6
Bjørnar Myrheim <45852703+Myrheimb@users.noreply.github.com>
2019-02-05 17:50:37 +0100

    Update server.py
    Added function that will navigate to today in the calendar view every morning.

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
-import eel
-import time
-import exchange
+import eel, time, exchange, os
+from apscheduler.schedulers.background import BackgroundScheduler
 
 # Initiates the web folder in the hierarchy.
 eel.init('web')
+
+# A function that triggers a JavaScript function that navigates to today in the calendar view.
+def nextDayPython():
+    eel.nextDay()
 
 # Get updated calendar information from Exchange every 5 seconds.
 def getExchangeEventsThread():
@@ -18,11 +21,20 @@ def roomAvailabilityThread():
         eel.sleep(5.0)
         exchange.roomAvailability()
 
+# Set up a scheduled event that runs nextDayPython daily at 5am.
+def nextDayThread():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(nextDayPython, 'cron', hour=5, minute=0)
+    scheduler.start()
+
 # Starts the function to get updated calendar information in its own thread.
 eel.spawn(getExchangeEventsThread)
 
 # Starts the function to check room availability in its own thread.
 eel.spawn(roomAvailabilityThread)
+
+# Starts the function to navigate to today in the calendar view in its own thread.
+eel.spawn(nextDayThread)
 
 # Settings to run Chrome in kiosk mode if eel.start is run with the web_app_options.
 web_app_options = {
